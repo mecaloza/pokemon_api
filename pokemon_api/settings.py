72a424c4e7a208011e10711095b7248bfefd7430
DEV Esteban L <lozadagiraldo.esteban111@gmail.com>
2021-07-06 01:18:44 -0500

    se arega configuracion de base de datos mysql

--- pokemon_api/settings.py
+++ pokemon_api/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = "pokemon_api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ebdb',
+        'USER': 'pokemondb',
+        'PASSWORD': 'est23loz55',
+        'HOST': 'aagd4fxsdq87wd.c2xacwacu1dj.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
